bcb38c021197ed580aca506ea8e64b5d4b4c4e91
Elsie <elsie19@pm.me>
2024-12-05 20:34:50 -0500

    broken

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
+use fluent_fallback::Localization;
 use fluent_resmgr::ResourceManager;
 use nu_plugin::{Plugin, PluginCommand};
+use std::sync::Mutex;
 
 mod commands;
 
 use crate::commands::print::Print;
 use crate::commands::register::Register;
 
-pub struct FluentPlugin;
+pub struct FluentPlugin {
+    msgr: Mutex<Localization<Vec<&'static str>, ResourceManager>>,
+}
 
 impl Plugin for FluentPlugin {
     fn version(&self) -> String {
